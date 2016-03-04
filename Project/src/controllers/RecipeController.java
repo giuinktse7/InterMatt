2827daf0673ccfa179924ed437a06b95d7730717
Mikael <git@evinja.se>
2016-03-04 07:51:02 +0100

    Controller to the new recipe pane

--- Project/src/controllers/RecipeController.java
+++ Project/src/controllers/RecipeController.java
@@ -2,35 +2,23 @@ package controllers;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
-import util.ShoppingCartHandler;
+import javafx.scene.control.Label;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import control.ModalPopup;
-
 public class RecipeController implements Initializable {
 
-    @FXML private ModalPopup bottomPane;
-    @FXML private Button closeButton;
-    @FXML private Pane contentPane;
-    @FXML private VBox tableBox;
-	
+	@FXML private Label pane_title;
+	public String user_first = "du";
+
+	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		setCloseAction();
+		pane_title.setText("Tack för ditt köp " + user_first + "!");
 	}
 
-	private void setCloseAction() {
-		bottomPane.setOnExit(() -> {
-			MainController.get().finishPurchase();
-			ShoppingCartHandler.getInstance().clearCart();
-		});
-		closeButton.setOnAction(e -> {
-			bottomPane.close();
-		});
-	}
+
+
+
 
 }
