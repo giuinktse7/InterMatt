496128e29307f6bdcf818b28b524f156c95bd68c
Mikael <git@evinja.se>
2016-03-04 13:17:58 +0100

    Validate all credentials

--- Project/src/controllers/CredentialsController.java
+++ Project/src/controllers/CredentialsController.java
@@ -6,12 +6,15 @@ import javafx.beans.binding.BooleanBinding;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
+import se.chalmers.ait.dat215.project.IMatDataHandler;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.regex.Pattern;
 
 public class CredentialsController implements Initializable {
+
+	private static IMatDataHandler db = IMatDataHandler.getInstance();
 
 	@FXML private TextField txtLastname;
 	@FXML private TextField txtFirstname;
@@ -21,41 +24,55 @@ public class CredentialsController implements Initializable {
 	@FXML private TextField txtCity;
 	@FXML private TextField txtEmail;
 
-	public String user_last = "", user_first = "", user_SSN = "", user_adress = "", user_postal = "", user_city= "", user_email= "";
-
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		
+		txtLastname.setText(db.getCustomer().getLastName());
+		txtFirstname.setText(db.getCustomer().getFirstName());
+		txtSSN.setText(db.getCustomer().getPhoneNumber());
+		txtAdress.setText(db.getCustomer().getAddress());
+		txtPostalcode.setText(db.getCustomer().getPostCode());
+		txtCity.setText(db.getCustomer().getPostAddress());
+
+
+		txtPostalcode.textProperty().addListener((observable, oldValue, newValue) -> {
+			if (!newValue.matches("\\d*")) {
+				txtPostalcode.setText(oldValue);
+				newValue = oldValue;
+			}
+			if (newValue.length() > 5){
+				txtPostalcode.setText(txtPostalcode.getText().substring(0,5));
+			}
+			if (newValue.length() == 5){
+				txtCity.requestFocus();
+			}
+		});
+
+		txtSSN.textProperty().addListener((observable, oldValue, newValue) -> {
+			if (!newValue.matches("\\d*")) {
+				txtSSN.setText(oldValue);
+				newValue = oldValue;
+			}
+			if (newValue.length() > 10){
+				txtPostalcode.setText(txtPostalcode.getText().substring(0,10));
+			}
+			if (newValue.length() == 10){
+				// select next button maybe..
+			}
+		});
+
 	}
-//
-//	public boolean verifyInput(){
-//		if (txtLastname.getText().isEmpty()) return false;
-//		if (txtFirstname.getText().isEmpty()) return false;
-//		if (txtSSN.getText().length() != 10 ||
-//				Integer.parseInt(txtSSN.getText().substring(0, 2)) <= 97) return false;
-//		if (txtAdress.getText().isEmpty()) return false;
-//		if (txtPostalcode.getText().length() != 5) return false;
-//		if (txtCity.getText().isEmpty()) return false;
-//		if (Pattern.matches("[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,4}", txtEmail.getText())) return false;
-//
-//		user_last = txtLastname.getText();
-//		user_first = txtFirstname.getText();
-//		user_SSN = txtSSN.getText();
-//		user_adress =txtAdress.getText();
-//		user_postal = txtPostalcode.getText();
-//		user_city = txtCity.getText();
-//		user_email = txtEmail.getText();
-//
-//		System.out.println("USER LAST: " + user_last);
-//
-//
-//		return true;
-//	}
 
 	public void save_user_data(){
-
+		db.getCustomer().setLastName(txtLastname.getText());
+		db.getCustomer().setFirstName(txtFirstname.getText());
+		db.getCustomer().setPhoneNumber(txtSSN.getText());
+		db.getCustomer().setAddress(txtAdress.getText());
+		db.getCustomer().setPostCode(txtPostalcode.getText());
+		db.getCustomer().setPostAddress(txtCity.getText());
 	}
+
+
 
 	private BooleanBinding emptyTextFieldBinding(TextField textField ) {
 		BooleanBinding binding = createBinding(textField, NON_EMPTY );
@@ -69,15 +86,66 @@ public class CredentialsController implements Initializable {
 		});
 		return binding;
 	}
+
+	private BooleanBinding ssnFieldBinding(TextField textField){
+		BooleanBinding binding = createBinding(textField, GOOD_SSN);
+		binding.addListener((obs, oldValue, newValue) -> {
+			if (!newValue) {
+				textField.getStyleClass().add("bad-input");
+			}else{
+				textField.getStyleClass().remove("bad-input");
+			}
+
+		});
+		return binding;
+	}
+
+	private BooleanBinding mailFieldBinding(TextField textField){
+		BooleanBinding binding = createBinding(textField, GOOD_MAIL);
+		binding.addListener((obs, oldValue, newValue) -> {
+			if (!newValue) {
+				textField.getStyleClass().add("bad-input");
+			}else{
+				textField.getStyleClass().remove("bad-input");
+			}
+
+		});
+		return binding;
+	}
+
+	private BooleanBinding postalFieldBinding(TextField textField){
+		BooleanBinding binding = createBinding(textField, GOOD_POSTAL);
+		binding.addListener((obs, oldValue, newValue) -> {
+			if (!newValue) {
+				textField.getStyleClass().add("bad-input");
+			}else{
+				textField.getStyleClass().remove("bad-input");
+			}
+
+		});
+		return binding;
+	}
+
+
 	
 	public BooleanBinding[] getBindings() {
 		return new BooleanBinding[] { emptyTextFieldBinding(txtLastname), emptyTextFieldBinding(txtFirstname),
-				emptyTextFieldBinding(txtSSN), emptyTextFieldBinding(txtAdress), emptyTextFieldBinding(txtPostalcode),
-				emptyTextFieldBinding(txtCity), emptyTextFieldBinding(txtEmail)
+				ssnFieldBinding(txtSSN), emptyTextFieldBinding(txtAdress), postalFieldBinding(txtPostalcode),
+				emptyTextFieldBinding(txtCity)
+				, mailFieldBinding(txtEmail)
 		};
 	}
 
+	private static final String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"  + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+	private Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+
+	private final VerifyTextField GOOD_MAIL = (txtField) -> ( pattern.matcher(txtField.getText()).matches()  );
+	private final VerifyTextField GOOD_POSTAL = (txtField) -> ( txtField.getText().length() == 5 );
+	private final VerifyTextField GOOD_SSN = (txtField) -> (txtField.getText().length() == 10 );
+	//|| Integer.parseInt(txtField.getText().substring(0, 2)) <= 97)
 	private final VerifyTextField NON_EMPTY = (txtField) -> ( !txtField.textProperty().get().equals("")) ;
+
+
 
 	private BooleanBinding createBinding (TextField textField, VerifyTextField verifyTextField){
 		return Bindings.createBooleanBinding(()->verifyTextField.verify(textField), textField.textProperty() );
